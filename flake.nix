0b027ef3791997a5bf9f4133066819b47d3d3175
lhohan <2922363+lhohan@users.noreply.github.com>
2024-09-27 14:38:56 +0200

    s: add work-shop runner binary see repo is independent now of checkout location

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  #inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [
+         "aarch64-darwin"
+         "aarch64-linux"
+         "x86_64-linux"
+         "x86_64-darwin"
+       ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.just
+            pkgs.cargo
+          ];
+        };
+      });
+}
